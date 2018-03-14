1ff9986d7338afb85373ac335349e65516df2d9e
LeoSHEIK <leonardosilva.r@hotmail.com>
2018-03-14 19:29:53 -0300

    Add files via upload

--- vrp_chatrules/__resource.lua
+++ vrp_chatrules/__resource.lua
@@ -2,6 +2,10 @@ description "vrp_chatrules"
 
 dependency "vrp"
 
+client_scripts{
+  "client.lua"
+}
+
 server_scripts{
   "@vrp/lib/utils.lua",
   "server.lua"
--- vrp_chatrules/server.lua
+++ vrp_chatrules/server.lua
@@ -1,3 +1,8 @@
+--[[
+Developed by Leo SHK
+Version 1.1
+]]--
+
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
 
@@ -7,6 +12,15 @@ vRPclient = Tunnel.getInterface("vRP","vRP_chatrules")
 local cfg = module("vrp_chatrules", "cfg/config")
 local cfgcmd = cfg.chatcmd
 local cfgrules = cfg.chatgroups
+local cfgu = cfg.chatuserdefault
+local cfgad = cfg.ad
+
+RegisterServerEvent('chatrules:ad')
+AddEventHandler('chatrules:ad', function()
+  local r = math.random(1, #cfgad)
+  TriggerClientEvent('chatMessage', -1, cfgad[r].display, { cfgad[r].r, cfgad[r].g, cfgad[r].b }, cfgad[r].text)
+end)
+
 
 
 AddEventHandler('chatMessage', function(source, name, msg)
@@ -16,12 +30,12 @@ AddEventHandler('chatMessage', function(source, name, msg)
   if user_id ~= nil then
     vRP.getUserIdentity({user_id, function (identity)
       sm = stringsplit(msg, " ");
-      CancelEvent()
 
       local iscmd = false
 
       for r=1, #cfgcmd do
         if sm[1] == cfgcmd[r].cmd then
+          print(cfgcmd[r].cmd)
           if vRP.hasPermission({user_id,cfgcmd[r].permission}) then
             iscmd = true
             if cfgcmd[r].userdb then
@@ -33,28 +47,37 @@ AddEventHandler('chatMessage', function(source, name, msg)
             if cfgcmd[r].anonymous then
               name = ""
             end
-            CancelEvent()
-            TriggerClientEvent('chatMessage', -1, cfgcmd[r].display .. name.." ", { cfgcmd[r].r, cfgcmd[r].g, cfgcmd[r].b }, string.sub(msg,string.len(cfgcmd[r].cmd)+1))
+            TriggerClientEvent('chatMessage', -1, cfgcmd[r].display.." " .. name.." ", { cfgcmd[r].r, cfgcmd[r].g, cfgcmd[r].b }, string.sub(msg,string.len(cfgcmd[r].cmd)+1))
           end
         end
       end
 
-      if not iscmd then
+      if string.find(sm[1], "/", 1) and not iscmd then
+        table.remove(sm, 1)
+        msg = nil
+      end
+
+      if not iscmd and msg ~= nil then
         local ing = false
         for v=1, #cfgrules do
           if vRP.hasPermission({user_id,cfgrules[v].permission}) then
             if cfgrules[v].userdb then
               local dbfirstname = identity.firstname
               local dbname = identity.name
-              local name = dbfirstname .." "..dbname
+              name = dbfirstname .." "..dbname
             end
             ing = true
-            CancelEvent()
-            TriggerClientEvent('chatMessage', -1, cfgrules[v].display .. name.." ", { cfgrules[v].r, cfgrules[v].g, cfgrules[v].b }, msg)
+            TriggerClientEvent('chatMessage', -1, cfgrules[v].display.." " .. name.." ", { cfgrules[v].r, cfgrules[v].g, cfgrules[v].b }, msg)
+
           end
         end
         if not ing then
-          TriggerClientEvent('chatMessage', -1, "Cidadão | " .. name.." ", { 239, 229, 255 }, msg)
+          if cfgu.userdb then
+            local dbfirstname = identity.firstname
+            local dbname = identity.name
+            name = dbfirstname .." "..dbname
+          end
+          TriggerClientEvent('chatMessage', -1, cfgu.display.." " .. name.." ", { cfgu.r, cfgu.g, cfgu.b }, msg)
         end
       end
     end})
